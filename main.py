a307b4443ef19e57879701b16df4222194d52c9b
Rishabh Mukund <rishabh.m96@gmail.com>
2021-11-15 12:05:19 -0500

    Fixed a few out of bound errors, optimised the code and included comments

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 # KUKA WAII Robot Model
 # Copyright (c) 2021 Rishabh Mukund
+# MIT License
 #
-#  Description: This code will generate the Forward and Inverse Kinematics for position and velocity and follow a
-#  given trajectory.
+# Description: This code will generate the Forward and Inverse Kinematics for position and velocity and follow a
+# given trajectory.
 
 
 # Importing all the required header files
-import time
-
-from sympy import cos, sin
+from sympy import cos, sin, pprint
 import numpy as np
 import sympy as sp
 import matplotlib.pyplot as plt
@@ -17,14 +16,14 @@ import matplotlib.pyplot as plt
 # @brief: A function which will generate transformation matrices with respect to the 0th frame
 #
 # @param: A vector of joint angles (1 * 6)
-# @return: A vector of transformation matrices with respect to the 0th frame (n * 4 * 4)
+# @return: A vector of transformation matrices with respect to the 0th frame (7 * 4 * 4)
 def calculate_tm(a, d1, d3, d5, d7):
     th = [sp.Matrix([[cos(a[0]), 0, -sin(a[0]), 0], [sin(a[0]), 0, cos(a[0]), 0], [0, -1, 0, d1], [0, 0, 0, 1]]),
           sp.Matrix([[cos(a[1]), 0, sin(a[1]), 0], [sin(a[1]), 0, -cos(a[1]), 0], [0, 1, 0, 0], [0, 0, 0, 1]]),
           sp.Matrix([[cos(a[2]), 0, -sin(a[2]), 0], [0, 1, 0, 0], [sin(a[2]), 0, cos(a[2]), d3], [0, 0, 0, 1]]),
           sp.Matrix([[cos(a[3]), 0, -sin(a[3]), 0], [sin(a[3]), 0, cos(a[3]), 0], [0, -1, 0, d5], [0, 0, 0, 1]]),
           sp.Matrix([[cos(a[4]), 0, sin(a[4]), 0], [sin(a[4]), 0, -cos(a[4]), 0], [0, 1, 0, 0], [0, 0, 0, 1]]),
-          sp.Matrix([[cos(a[5]), -sin(a[5]), 0, 0], [sin(a[5]), cos(a[5]), 0, 0], [0, 0, 1, d7], [0, 0, 0, 1]])]
+          sp.Matrix([[cos(a[5]), -sin(a[5]), 0, 0], [sin(a[5]), cos(a[5]), 0, 0], [0, 1, 0, d7], [0, 0, 0, 1]])]
 
     # Initializing transformation vector with respect to 0th frame
     th0n_temp = [sp.Matrix([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])]
@@ -37,19 +36,18 @@ def calculate_tm(a, d1, d3, d5, d7):
 
 # @brief: A function to calculate the jacobian matrix for each joint
 #
-# @param: A vector of transformation matrices with respect to the 0th frame (6 * 4 * 4)
+# @param: A vector of transformation matrices with respect to the 0th frame (7 * 4 * 4)
 # @return: Jacbian matrices (6 * 6)
 def calculate_jacobian(th0n_temp):
-    j_temp = []                                       # To extract the Z component of R matrix
-    d06 = th0n_temp[-1].extract([0, 1, 2], [-1])      # Extraxting the distance from Transformation Matrix
-    for i in th0n_temp:
-        ang_vel = i.extract([0, 1, 2], [2])           # Extracting the Z component
-        d0i = i.extract([0, 1, 2], [-1])              # Extracting the o matrix
+    j_temp = []
+    d06 = th0n_temp[-1].extract([0, 1, 2], [-1])                 # Extraxting the dn from Transformation Matrix
+    for i in range(0, 6):
+        ang_vel = th0n_temp[i].extract([0, 1, 2], [2])           # Extracting the Z component
+        d0i = th0n_temp[i].extract([0, 1, 2], [-1])              # Extracting the di matrix
         lin_vel = ang_vel.cross(d06 - d0i)
-        j0n = lin_vel.col_join(ang_vel)               # Concatinating lin and ang velocity for a joint
+        j0n = lin_vel.col_join(ang_vel)                          # Concatinating lin and ang velocity for a joint
         j_temp.append(j0n)
-
-    j_1 = j_temp[0]                                   # Concatinating for J matrix
+    j_1 = j_temp[0]                                              # Concatinating for J matrix
     for x in range(1, 6):
         j_1 = j_1.row_join(j_temp[x])
     return j_1
@@ -57,7 +55,7 @@ def calculate_jacobian(th0n_temp):
 
 # @brief: A function to plot the robotic arm
 #
-# @param: A vector of transformation matrices with respect to the 0th frame (n * 4 * 4)
+# @param: A vector of transformation matrices with respect to the 0th frame (7 * 4 * 4)
 def plot_arm(th0n_temp):
     plot_line(th0n_temp[0], th0n_temp[1])
     plot_line(th0n_temp[1], th0n_temp[2])
@@ -96,17 +94,18 @@ def plot_frame(f):
     ax.plot3D([tx, 3 * rz[0, 0] + tx], [ty, 3 * rz[1, 0] + ty], [tz, 3 * rz[2, 0] + tz], 'blue')
 
 
-# @brief: A function to generate and plot a trajectory of a circle
+# @brief: A function to generate and plot a trajectory of a circle in XZ plane
 #
-# @param: x_off, y_off, z_off are the center of the circle in 3D plane, r is the raduis of the circle and
-# s is the precision of the circle
+# @param: x_off, y_off, z_off - are the center of the circle in 3D plane,
+#         r - the raduis of the circle and
+#         s - the precision of the circle
 def circle(x_off, y_off, z_off, r, s):
-    th1 = np.linspace(0, 2 * 3.14, s)
+    th1 = np.linspace(0, 2 * 3.14, s)              # Equally spaced angles of a circle
     x_val = []
     z_val = []
     for i in th1:
-        x_val.append(r * sin(i) + x_off)
-        z_val.append(r * cos(i) + z_off)
+        x_val.append(r * sin(i) + x_off)           # Vector of X Cordinates
+        z_val.append(r * cos(i) + z_off)           # Vector of Y Cordinates
     y_val = np.ones(s) * y_off
     return x_val, y_val, z_val
 
@@ -123,37 +122,37 @@ if __name__ == '__main__':
 
     # calculating transformation matrices with respect to 0th frame
     tm0n = calculate_tm(ja, d1, d3, d5, d7)
-    print(tm0n)
 
     # calculating the jacobian matrix
     j = calculate_jacobian(tm0n)
-    print(j)
+    pprint(j)
 
-    # plotting the arm and path
+    # plotting the arm
     plt.figure()
     ax = plt.axes(projection='3d')
     plot_arm(tm0n)
 
+    # Getting the trajectory of the circle
     # x, y, z = circle(120, 0, 1015, 100, 100)
     x, y, z = circle(0, 605, 680, 100, 100)
 
+    # Plotting the circle
     ax.plot3D(x, y, z, 'yo')
 
+    delta_time = 5 / len(x)         # Total time by number of points to cover
+    print(delta_time)
     # Code to follow the trajectory
-    for k in range(0, 100):
-        curr_pos = tm0n[-1].extract([0, 1, 2], [-1])
-        req_pos = sp.Matrix([[x[k]], [y[k]], [z[k]]])
-        delta_pos = req_pos - curr_pos
-        delta_angle = j.inv() * delta_pos.col_join(sp.Matrix([[0], [0], [0]]))
-        for m in range(0,6):
-            ja[m] = ja[m] + delta_angle[0]
+    for k in range(0, len(x)):
+        curr_pos = tm0n[-1].extract([0, 1, 2], [-1])                              # The position of the end effector
+        req_pos = sp.Matrix([[x[k]], [y[k]], [z[k]]])                             # The required position
+        rate_pos = (req_pos - curr_pos) / delta_time                              # Rate of change in the position
+        rate_angle = j.inv() * rate_pos.col_join(sp.Matrix([[0], [0], [0]]))      # Rate of change in angles
+        for m in range(0, 6):
+            ja[m] = ja[m] + (rate_angle[m] * delta_time)                          # Updating joint angles
         print(ja)
-        tm0n = calculate_tm(ja, d1, d3, d5, d7)
-        j = calculate_jacobian(tm0n)
-        plot_arm(tm0n)
-        plt.pause(0.001)
+        tm0n = calculate_tm(ja, d1, d3, d5, d7)                                   # FK for new position of the arm
+        j = calculate_jacobian(tm0n)                                              # Calculate the new jacobian
+        plot_arm(tm0n)                                                            # Plot arm in 3D space
+        plt.pause(delta_time)
 
     plt.show()
-
-
-
